afc0628f3b096a7ca4117f88856e996ba345045c
AlejandroFabianCampos <alejandrofabiancampos2000@gmail.com>
2019-08-06 23:25:05 -0300

    Uploading to heroku pt.2

--- app/Http/Controllers/importController.php
+++ app/Http/Controllers/importController.php
@@ -11,7 +11,7 @@ class importController extends Controller
 {
     public function import(Request $request) 
     {
-        Excel::import(new infoImport, $request->file('import_file'));
+        Excel::import(new infoImport(), $request->file('import_file'));
         
         return redirect('/')->with('success', 'All good!');
     }
