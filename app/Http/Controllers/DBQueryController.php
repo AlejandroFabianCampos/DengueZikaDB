3f9d5b81e435cde63aa93c19b027b27d8e5d6256
AlejandroFabianCampos <alejandrofabiancampos2000@gmail.com>
2019-08-16 23:02:52 -0300

    Removed unneeded comments from the query controller

--- app/Http/Controllers/DBQueryController.php
+++ app/Http/Controllers/DBQueryController.php
@@ -23,12 +23,6 @@ class DBQueryController extends Controller
         $tucuman = $request->input('Tucuman');
         $enfermedad = $request->input('enfermedad');
         
-        $vacio;
-        
-        $variableFalsa = DB::table('events');
-        /*if (true == true) {
-            $variableFalsa = true;
-        }*/
 
         function globalQueryWithoutUnion($province, $enfLocal) {
             return DB::table('events')
@@ -38,13 +32,6 @@ class DBQueryController extends Controller
                 });
                 
         }
-
-        /*function globalQueryWithUnion($province, $enfLocal, $vacioLocal) {
-            return DB::table('events')
-                ->where('province_name', $province)
-                ->where('disease', $enfLocal)
-                ->union($vacioLocal);
-        }*/
 
         function returnProvinceTable ($province, $enfermedad) {
             if ($province) {
@@ -101,46 +88,6 @@ class DBQueryController extends Controller
                 return $query->union($tucumanData);
             });
 
-        /*if ($buenosAires) {
-            $newQuery = globalQueryWithoutUnion($buenosAires, $enfermedad);
-            $vacio = $newQuery;
-        }
-
-        if ($caba) {
-            if ($vacio) {
-                $newQuery = globalQueryWithUnion($caba, $enfermedad, $vacio, $variableFalsa);
-            } else {
-                $newQuery = globalQueryWithoutUnion($caba, $enfermedad);
-            }
-            
-            $vacio = $newQuery;
-        }
-
-        if ($chaco) {
-            if ($vacio) {
-                $newQuery = globalQueryWithUnion($chaco, $enfermedad, $vacio);
-            } else {
-                $newQuery = globalQueryWithoutUnion($chaco, $enfermedad);
-            }
-            
-            $vacio = $newQuery;
-        }*/
-        
-        /*if ($caba) {
-            $newQuery = globalQueryWithPossibleUnion($caba, $enfermedad, $vacio);
-            $vacio = $newQuery;
-        }
-
-        if ($caba) {
-            $newQuery = globalQueryWithPossibleUnion($caba, $enfermedad, $vacio);
-            $vacio = $newQuery;
-        }
-
-        if ($caba) {
-            $newQuery = globalQueryWithPossibleUnion($caba, $enfermedad, $vacio);
-            $vacio = $newQuery;
-        }*/
-
         
 
         if ($enfermedad){
@@ -180,65 +127,6 @@ class DBQueryController extends Controller
         }
         
         
-
-            
-
-        /*$events = DB::table('events')
-            ->when($buenosAires, function ($query) {
-                $final = DB::table('events')
-                    ->where('province_name', 'Buenos Aires')
-                    ->union($query);
-                return $final;
-            })
-            ->when($caba, function ($query) {
-                $final = DB::table('events')
-                    ->where('province_name', 'CABA')
-                    ->union($query);
-                return $final;
-            })
-            ->when($chaco, function ($query) {
-                $final = DB::table('events')
-                    ->where('province_name', 'Chaco')
-                    ->union($query);
-                return $final;
-            })
-            ->when($cordoba, function ($query) {
-                return DB::table('events')->where('province_name', 'Córdoba');
-            })
-            ->when($corrientes, function ($query) {
-                return DB::table('events')->where('province_name', 'Corrientes');
-            })
-            ->when($entreRios, function ($query) {
-                return DB::table('events')->where('province_name', 'Entre Ríos');
-            })
-            ->when($formosa, function ($query) {
-                return DB::table('events')->where('province_name', 'Formosa');
-            })
-            ->when($misiones, function ($query) {
-                return DB::table('events')->where('province_name', 'Misiones');
-            })
-            ->when($salta, function ($query) {
-                return DB::table('events')->where('province_name', 'Salta');
-            })
-            ->when($santaFe, function ($query) {
-                return DB::table('events')->where('province_name', 'Santa Fe');
-            })
-            ->when($santiagoDelEstero, function ($query) {
-                return DB::table('events')->where('province_name', 'Santiago del Estero');
-            })
-            ->when($tucuman, function ($query) {
-                return DB::table('events')->where('province_name', 'Tucumán');
-            })
-            ->when($enfermedad, function ($query, $enfermedad) {
-                return $query->where('disease', $enfermedad);
-                
-                    ->when($variableFalsa, function ($query) {
-                        return $query->get();
-                    });
-            })
-            ->get();
-        */
-
         return view('baseDeDatos', ['events' => $events, 'quantityEvents'=> count($events)]);//->with("events", $events);
     }
 
